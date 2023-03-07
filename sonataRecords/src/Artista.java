83574e6a4acbfa23824d4742fc68dd81e1636119
lrmello <leonardo.reis28051995@gmail.com>
2023-03-07 09:43:29 -0300

    Adição do arquivo Artista.java com seus getters, ajuste do arquivo Midia.java

---
+++ sonataRecords/src/Artista.java
@@ -0,0 +1,13 @@
+package sonataRecords.src;
+
+public class Artista {
+    private String nome;
+
+    public Artista(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+}
--- sonataRecords/src/Midia.java
+++ sonataRecords/src/Midia.java
@@ -1,4 +1,31 @@
 package sonataRecords.src;
+//A classe midia será responsável por armazenar todos os dados do artista
 public class Midia {
+    private String titulo;
+    private int ano;
+    private String genero;
+    private Artista artista;
 
+    public void midia(String titulo, int duracao, String genero){
+        this.titulo = titulo;
+        this.ano = ano;
+        this.genero = genero;
+        this.artista = artista;
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public int getAno(){
+        return ano;
+    }
+
+    public String getGenero(){
+        return genero;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
 }
